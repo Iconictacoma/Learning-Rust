69a5277bf6276b99f770df19e59964dcab4cdbdb
Iconictacoma <sevonprince371@gmail.com>
2026-05-26 08:18:25 -0400

    Create notoldenough.rs

---
+++ notoldenough.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let age = 16;
+    
+    if age >= 18 {
+    println!("You are old enough to vote");
+    } else {
+    println!("You arent old enough to vote, come back when you are older...")
+    }
+}
+    
